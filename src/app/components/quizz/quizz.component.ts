aea741cf199227a2acccb2d10e688cc563ce08c9
Jean Carlo <jeansanto2002@gmail.com>
2024-08-06 10:47:17 -0300

    Criando as regras do jogo

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quizz_questions from "../../../assets/data/quizz_questions.json"
 
 @Component({
   selector: 'app-quizz',
@@ -20,7 +21,28 @@ export class QuizzComponent implements OnInit {
   finished: boolean = false
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    if (quizz_questions) {
+      this.finished = false
+      this.title = quizz_questions.title
+      this.questions = quizz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+    }
+  }
+
+  playerChoose(value: string) {
+    this.answers.push(value)
+  }
+
+  async nextStep() {
+    this.questionIndex++
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex]
+    } else {
+      this.finished = true
+    }
   }
 
 }
